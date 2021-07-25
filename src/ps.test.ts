836a08d4a0dceff35aaba866d1a8265e3e939d26
Nikolay Makhonin <nikolay.makhonin@gmail.com>
2021-07-25 06:31:44 +0300

    test: Improved tests

--- src/ps.test.ts
+++ src/ps.test.ts
@@ -57,7 +57,7 @@ describe('ps', function () {
 
 		const findProcs = result.filter(o => o.command.indexOf(command) >= 0)
 		assert.ok(findProcs)
-		assert.strictEqual(findProcs.length, 1, 'findProcs.length=' + JSON.stringify(findProcs))
+		assert.strictEqual(findProcs.length, 1, 'findProcs.length=' + JSON.stringify(findProcs, null, 4))
 		assert.strictEqual(findProcs[0].pid, proc.pid)
 
 		process.kill(findProcs[0].pid, 'SIGINT')
@@ -140,7 +140,7 @@ describe('ps', function () {
 
 		const findProcs = Object.values(result).filter(o => o.command.indexOf(command) >= 0)
 		assert.ok(findProcs)
-		assert.strictEqual(findProcs.length, 1, 'findProc.length=' + JSON.stringify(findProcs))
+		assert.strictEqual(findProcs.length, 1, 'findProc.length=' + JSON.stringify(findProcs, null, 4))
 		assert.strictEqual(findProcs[0].pid, proc.pid)
 
 		process.kill(findProcs[0].pid, 'SIGINT')
